1d2360165e4fbbe0fdb4dcea675507c2300a50fb
Cristian Lopez <cristianlopezr.931@gmail.com>
2022-05-15 19:37:05 -0400

    access control allow

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ db.on('error', () => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, 	X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-	Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, 	DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 const productosRouter = require('./routes/productos.routes');
 app.use('/productos', productosRouter);
